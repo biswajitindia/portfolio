0be237f48f59c652bed074a58b0cbbda358ae401
biswajitindia <biswajitsahu1472@gmail.com>
2024-12-31 13:02:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const box= document.getElementById("box")
+
+function changeColor() {
+    
+    box.style.backgroundColor="red"
+    
+}
+function black() {
+
+    box.style.backgroundColor="black"
+    
+}
+function blue() {
+
+    box.style.backgroundColor="blue"
+    
+}
